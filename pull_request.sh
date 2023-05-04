ef2ffdffacf6517adf65c702976931455bb6a778
Allan <user@NBVIX014631.ISH.LOCAL>
2023-05-04 19:29:30 -0300

    style: :umbrella: change bash file

--- pull_request.sh
+++ pull_request.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-LOGS=`git log $(git merge-base main develop) develop --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit`
+# ano-mes-dia
+# git log --since="2023-05-04T18:35:00"
+
+# LOGS=`git log --oneline main..develop --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit`
+# LOGS=`git log main..develop --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative`
+LOGS=`git log origin/main..HEAD --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --no-merges`
 echo "${LOGS}"
 
-# gh pr create --base develop --head --title "PULL REQUEST" --body "$LOGS"
-gh pr create --title "PULL REQUEST" --fill
+# main -> develop
+gh pr create --base $1 --head $2 --title "PULL REQUEST" --body "$LOGS"
+# gh pr create --title "PULL REQUEST" --fill
+# git log --oneline --cherry main...develop
+
+
+# git log --oneline --raw -m
