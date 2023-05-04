8fbae3b860d1f35fded5006f095a5a95a2f2b0e5
Allan <user@NBVIX014631.ISH.LOCAL>
2023-05-04 18:33:30 -0300

    feat: :sparkles: add

--- pull_request.sh
+++ pull_request.sh
@@ -3,4 +3,5 @@
 LOGS=`git log $(git merge-base main develop) develop --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit`
 echo "${LOGS}"
 
-gh pr create --base master --title "PULL REQUEST" --body "$LOGS"
+# gh pr create --base master --title "PULL REQUEST" --body "$LOGS"
+gh pr create --base master -f
